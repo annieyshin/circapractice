ff764c0429fddfe75245050887c5fc362d1202cf
Elly Maimon <eliranmaimon@gmail.com>
2018-11-01 13:00:39 -0700

    Create a home page view for the main path

---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+const HomePage = ({history}) => {
+  return (
+    <div style={{margin: "50px 50px"}}>
+      <h1>Homepage with maybe a login screen</h1>
+      <Button
+        size="massive"
+        primary
+        onClick={() => history.push("/reports")}
+      >
+      Go to reports!
+      </Button>
+    </div>
+  )
+}
+
+export default HomePage
