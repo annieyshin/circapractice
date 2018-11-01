ac64f5f17b96d454ed1730d4c7dd34c5460a89e5
Elly Maimon <eliranmaimon@gmail.com>
2018-11-01 13:00:22 -0700

    Add nav links from react-router to the nav bar, remove unnecessary state

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,52 +1,26 @@
 import React, { Component } from "react";
 import { Menu } from "semantic-ui-react";
+import { NavLink, withRouter, Link } from "react-router-dom";
 
 class NavBar extends Component {
-  state = {};
-
-  handleItemClick = (e, { name }) => this.setState({ activeLink: name });
-
   render() {
-    const { activeLink } = this.state;
-
     return (
       <div>
-        <Menu>
-          <Menu.Item>
+        <Menu fixed="top">
+          <Menu.Item as={Link} to="/" name="Circa">
             <img
               src="./logo.png"
               alt="logo"
               style={{ height: "100px", width: "100%" }}
             />
           </Menu.Item>
-
-          <Menu.Item
-            name="reports"
-            active={activeLink === "reports"}
-            onClick={this.handleItemClick}
-          >
-            Reports
-          </Menu.Item>
-
-          <Menu.Item
-            name="expenses"
-            active={activeLink === "expenses"}
-            onClick={this.handleItemClick}
-          >
-            Expenses
-          </Menu.Item>
-
-          <Menu.Item
-            name="projects"
-            active={activeLink === "projects"}
-            onClick={this.handleItemClick}
-          >
-            Projects
-          </Menu.Item>
+          <Menu.Item as={NavLink} to="/reports" name="Reports" />
+          <Menu.Item as={NavLink} to="/expenses" name="Expenses" />
+          <Menu.Item as={NavLink} to="/projects" name="Projects" />
         </Menu>
       </div>
     );
   }
 }
 
-export default NavBar;
+export default withRouter(NavBar);
