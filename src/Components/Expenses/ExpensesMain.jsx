737efc3cbf9d48bd90dcf30a376d2413250699e7
Elly Maimon <eliranmaimon@gmail.com>
2018-11-02 16:39:35 -0700

    Remove grid structure & add the expenses form component to expenses main

--- src/Components/Expenses/ExpensesMain.jsx
+++ src/Components/Expenses/ExpensesMain.jsx
@@ -1,22 +1,19 @@
 import React from "react";
-import { Grid, Button, Header, Icon } from "semantic-ui-react";
+import { Button, Header, Icon } from "semantic-ui-react";
 import ExpensesList from "./ExpensesList";
+import ExpensesForm from "./ExpensesForm";
 
 const ExpensesMain = () => {
   return (
-    <Grid columns={2}>
-      <Grid.Column width={10}>
-        <Header size="huge">
-          <Icon name="money bill alternate outline" />
-          <Header.Content>Expenses</Header.Content>
-        </Header>
-        <ExpensesList />
-        <Button content="Add Expense" icon="add" primary />
-      </Grid.Column>
-      <Grid.Column width={6}>
-        add new expense form to display on click of add expense button
-      </Grid.Column>
-    </Grid>
+    <div>
+      <Header size="huge">
+        <Icon name="money bill alternate outline" />
+        <Header.Content>Expenses</Header.Content>
+      </Header>
+      <ExpensesList />
+      <Button content="Add Expense" icon="add" primary style={{marginBottom: "2em"}}/>
+      <ExpensesForm />
+    </div>
   );
 };
 
