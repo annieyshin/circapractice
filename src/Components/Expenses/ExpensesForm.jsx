fd1df54e98b73bc58be65838aa5f20531c458239
Elly Maimon <eliranmaimon@gmail.com>
2018-11-02 13:57:58 -0700

    Create the ExpensesForm component

---
+++ src/Components/Expenses/ExpensesForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Form, Button } from 'semantic-ui-react';
+
+const ExpensesForm = () => {
+  return (
+    <div>
+      <h1>Expenses Form</h1>
+    </div>
+  )
+}
+
+export default ExpensesForm;
