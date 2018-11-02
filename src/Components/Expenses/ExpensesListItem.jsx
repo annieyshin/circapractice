384dfe6bbf39f5001c9a35afc3cd25ce07f812cb
Elly Maimon <eliranmaimon@gmail.com>
2018-11-02 16:17:13 -0700

    Edit to reformat for table instead of segments

--- src/Components/Expenses/ExpensesListItem.jsx
+++ src/Components/Expenses/ExpensesListItem.jsx
@@ -1,19 +1,24 @@
-import React from "react";
-import { Segment, Icon } from "semantic-ui-react";
+import React, { Component } from "react";
+import { Icon, Table } from "semantic-ui-react";
 
-const ExpensesListItem = () => {
-  return (
-    <Segment.Group horizontal>
-      <Segment>Example Expense</Segment>
-      <Segment>Amount in $$</Segment>
-      <Segment>
-        <Icon name="edit" />
-      </Segment>
-      <Segment>
-        <Icon name="delete" />
-      </Segment>
-    </Segment.Group>
-  );
+const listIcons = {
+  marginLeft: "1em",
 };
+
+export class ExpensesListItem extends Component {
+  render() {
+    const { title, cost } = this.props;
+    return (
+      <Table.Row>
+        <Table.Cell>{title}</Table.Cell>
+        <Table.Cell>${cost}</Table.Cell>
+        <Table.Cell>
+          <Icon name="edit" color="grey" style={listIcons} />
+          <Icon name="delete" color="red" style={listIcons} />
+        </Table.Cell>
+      </Table.Row>
+    );
+  }
+}
 
 export default ExpensesListItem;
