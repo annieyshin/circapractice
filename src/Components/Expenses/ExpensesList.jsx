17a5e6f9caed2ecfec828358d7ce674fa7c0d8d8
Elly Maimon <eliranmaimon@gmail.com>
2018-11-02 16:16:36 -0700

    Add table to organize the expenses

--- src/Components/Expenses/ExpensesList.jsx
+++ src/Components/Expenses/ExpensesList.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import ExpensesListItem from "./ExpensesListItem";
-import { Segment } from "semantic-ui-react";
+import { Table } from "semantic-ui-react";
+import { mockExpenses } from "../../Data/mockExpenses";
 
-const ExpensesList = () => {
-  return (
-    <Segment.Group vertical compact>
-      <ExpensesListItem />
-    </Segment.Group>
-  );
-};
+class ExpensesList extends Component {
+  render() {
+    return (
+      <Table columns={3} collapsing>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Title</Table.HeaderCell>
+            <Table.HeaderCell>Cost</Table.HeaderCell>
+            <Table.HeaderCell>Actions</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {mockExpenses.map(expense => (
+            <ExpensesListItem
+              key={expense.id}
+              title={expense.title}
+              cost={expense.cost}
+            />
+          ))}
+        </Table.Body>
+        <Table.Footer>
+          <Table.Row>
+            <Table.HeaderCell />
+            <Table.HeaderCell>Total: $500</Table.HeaderCell>
+            <Table.HeaderCell />
+          </Table.Row>
+        </Table.Footer>
+      </Table>
+    );
+  }
+}
 
 export default ExpensesList;
