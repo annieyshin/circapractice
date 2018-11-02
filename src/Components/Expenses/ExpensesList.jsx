9dc5512eb37f12a2a487c72771c5b32a68aa1674
Elly Maimon <eliranmaimon@gmail.com>
2018-11-02 13:58:15 -0700

    Create the ExpensesList component with a segment

---
+++ src/Components/Expenses/ExpensesList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Segment } from 'semantic-ui-react';
+
+const ExpensesList = () => {
+  return (
+    <Segment compact>
+      Expense List containing Expense List Items
+    </Segment>
+  )
+}
+
+export default ExpensesList
