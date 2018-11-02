022e5a19d9ad4a8b53bb07c8c786f3c2103c785c
Elly Maimon <eliranmaimon@gmail.com>
2018-11-02 15:17:04 -0700

    Create mock expenses to test rendering of data

---
+++ src/Data/mockExpenses.js
@@ -0,0 +1,17 @@
+export const mockExpenses = [
+  {
+    id: "1",
+    title: "Software",
+    cost: 500
+  },
+  {
+    id: "2",
+    title: "Coffee",
+    cost: 200
+  },
+  {
+    id: "3",
+    title: "Office Rent and Insurance",
+    cost: 1800
+  }
+];
