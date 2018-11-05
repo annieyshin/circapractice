8150936d9ee681af4c468af30c5a69f7df0af7dc
Elly Maimon <eliranmaimon@gmail.com>
2018-11-05 12:46:59 -0800

    Convert expenses list item into a functional comp, add props to argument

--- src/Components/Expenses/ExpensesListItem.jsx
+++ src/Components/Expenses/ExpensesListItem.jsx
@@ -1,24 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 import { Icon, Table } from "semantic-ui-react";
 
 const listIcons = {
-  marginLeft: "1em",
+  marginLeft: "1em"
 };
 
-export class ExpensesListItem extends Component {
-  render() {
-    const { title, cost } = this.props;
-    return (
-      <Table.Row>
-        <Table.Cell>{title}</Table.Cell>
-        <Table.Cell>${cost}</Table.Cell>
-        <Table.Cell>
-          <Icon name="edit" color="grey" style={listIcons} />
-          <Icon name="delete" color="red" style={listIcons} />
-        </Table.Cell>
-      </Table.Row>
-    );
-  }
-}
+const ExpensesListItem = ({title, cost}) => {
+  return (
+    <Table.Row>
+      <Table.Cell>{title}</Table.Cell>
+      <Table.Cell>${cost}</Table.Cell>
+      <Table.Cell>
+        <Icon name="edit" color="grey" style={listIcons} />
+        <Icon name="delete" color="red" style={listIcons} />
+      </Table.Cell>
+    </Table.Row>
+  );
+};
 
 export default ExpensesListItem;
