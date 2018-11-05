0a87d69250d91b051bc0ba4415c97981b19a659d
Elly Maimon <eliranmaimon@gmail.com>
2018-11-05 12:33:48 -0800

    Convert main expenses component into functional component to implement hooks

--- src/Components/Expenses/ExpensesMain.jsx
+++ src/Components/Expenses/ExpensesMain.jsx
@@ -1,27 +1,27 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Button, Header, Icon } from "semantic-ui-react";
 import ExpensesList from "./ExpensesList";
 import ExpensesForm from "./ExpensesForm";
 
-class ExpensesMain extends Component {
-  render() {
-    return (
-      <div>
-        <Header size="huge">
-          <Icon name="money bill alternate outline" />
-          <Header.Content>Expenses</Header.Content>
-        </Header>
-        <ExpensesList />
-        <Button
-          content="Add Expense"
-          icon="add"
-          primary
-          style={{ marginBottom: "2em" }}
-        />
-        <ExpensesForm />
-      </div>
-    );
-  }
-}
+const ExpensesMain = () => {
+  const [showForm, toggleForm] = useState(false);
+  return (
+    <div>
+      <Header size="huge">
+        <Icon name="money bill alternate outline" />
+        <Header.Content>Expenses</Header.Content>
+      </Header>
+      <ExpensesList />
+      <Button
+        onClick={() => toggleForm(showForm ? false : true)}
+        content="Add Expense"
+        icon="add"
+        primary
+        style={{ marginBottom: "2em" }}
+      />
+      {showForm && <ExpensesForm />}
+    </div>
+  );
+};
 
 export default ExpensesMain;
