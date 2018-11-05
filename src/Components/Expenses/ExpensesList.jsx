047dd7030df4b136c1e9b263bf9713206544488b
Elly Maimon <eliranmaimon@gmail.com>
2018-11-05 12:46:34 -0800

    Convert expenses list into a functional component

--- src/Components/Expenses/ExpensesList.jsx
+++ src/Components/Expenses/ExpensesList.jsx
@@ -1,38 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
 import ExpensesListItem from "./ExpensesListItem";
 import { Table } from "semantic-ui-react";
 import { mockExpenses } from "../../Data/mockExpenses";
 
-class ExpensesList extends Component {
-  render() {
-    return (
-      <Table columns={3}>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Title</Table.HeaderCell>
-            <Table.HeaderCell>Cost</Table.HeaderCell>
-            <Table.HeaderCell>Actions</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {mockExpenses.map(expense => (
-            <ExpensesListItem
-              key={expense.id}
-              title={expense.title}
-              cost={expense.cost}
-            />
-          ))}
-        </Table.Body>
-        <Table.Footer>
-          <Table.Row>
-            <Table.HeaderCell />
-            <Table.HeaderCell>Total: $500</Table.HeaderCell>
-            <Table.HeaderCell />
-          </Table.Row>
-        </Table.Footer>
-      </Table>
-    );
-  }
-}
+const ExpensesList = () => {
+  return (
+    <Table columns={3}>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>Title</Table.HeaderCell>
+          <Table.HeaderCell>Cost</Table.HeaderCell>
+          <Table.HeaderCell>Actions</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {mockExpenses.map(expense => (
+          <ExpensesListItem
+            key={expense.id}
+            title={expense.title}
+            cost={expense.cost}
+          />
+        ))}
+      </Table.Body>
+      <Table.Footer>
+        <Table.Row>
+          <Table.HeaderCell />
+          <Table.HeaderCell>Total: $500</Table.HeaderCell>
+          <Table.HeaderCell />
+        </Table.Row>
+      </Table.Footer>
+    </Table>
+  );
+};
 
 export default ExpensesList;
