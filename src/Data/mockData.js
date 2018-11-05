7c4aa116d6063b8b0b22092931056ab3720c5c7b
Elly Maimon <eliranmaimon@gmail.com>
2018-11-05 15:22:14 -0800

    Add mock projects to mock data

---
+++ src/Data/mockData.js
@@ -0,0 +1,60 @@
+export const mockExpenses = [
+  {
+    id: '1',
+    title: 'Software',
+    cost: 500
+  },
+  {
+    id: '2',
+    title: 'Coffee',
+    cost: 200
+  },
+  {
+    id: '3',
+    title: 'Office Rent and Insurance',
+    cost: 1800
+  }
+];
+
+
+export const mockProjects = [
+  {
+    id: 1,
+    wid: 1,
+    cid: 1,
+    name: 'A Project',
+    billable: true,
+    is_private: true,
+    active: true,
+    at: '2013-03-06T12:15:37+00:00',
+    template: true,
+    color: '5',
+    billingType: 'hourly'
+  },
+  {
+    id: 2,
+    wid: 1,
+    cid: 2,
+    name: 'Some Other Project',
+    billable: false,
+    is_private: true,
+    active: true,
+    at: '2013-03-06T12:15:37+00:00',
+    template: true,
+    color: '5',
+    billingType: 'hourly'
+  },
+  {
+    id: 3,
+    wid: 1,
+    cid: 2,
+    name: 'Project that is easy!',
+    billable: true,
+    is_private: true,
+    active: true,
+    at: '2013-03-06T12:15:37+00:00',
+    template: true,
+    color: '5',
+    billingType: 'hourly'
+  }
+];
