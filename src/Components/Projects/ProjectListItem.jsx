69ca9878cafb5330a4e1fb4b03f117cd98a5f4ca
Elly Maimon <eliranmaimon@gmail.com>
2018-11-06 10:36:43 -0800

    Switch client and billing cycle table columns around, fix import statement

--- src/Components/Projects/ProjectListItem.jsx
+++ src/Components/Projects/ProjectListItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Table } from 'semantic-ui-react';
-import BudgetType from './BudgetType';
+import BudgetTypeSelection from './BudgetTypeSelection';
 
 const ProjectListItem = ({ project }) => {
   const { name, billable, budgetType, cid } = project;
@@ -8,8 +8,8 @@ const ProjectListItem = ({ project }) => {
     <Table.Row>
       <Table.Cell>{name}</Table.Cell>
       <Table.Cell>{billable ? 'Yes' : 'No'}</Table.Cell>
-      <Table.Cell><BudgetType budgetType={budgetType} /></Table.Cell>
-      <Table.Cell>{cid}</Table.Cell>
+      <Table.Cell>{`Angel's Envy ${cid}`}</Table.Cell>
+      <Table.Cell><BudgetTypeSelection budgetType={budgetType} /></Table.Cell>
     </Table.Row>
   );
 };
