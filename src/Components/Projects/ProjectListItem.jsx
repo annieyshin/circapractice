ede44310de1277814f6c8f48d78c00084e64e289
Elly Maimon <eliranmaimon@gmail.com>
2018-11-05 16:44:29 -0800

    Add dropdown component for budget type in project list item

--- src/Components/Projects/ProjectListItem.jsx
+++ src/Components/Projects/ProjectListItem.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Table } from 'semantic-ui-react';
+import BudgetType from './BudgetType';
 
 const ProjectListItem = ({ project }) => {
   const { name, billable, budgetType, cid } = project;
@@ -7,7 +8,7 @@ const ProjectListItem = ({ project }) => {
     <Table.Row>
       <Table.Cell>{name}</Table.Cell>
       <Table.Cell>{billable ? 'Yes' : 'No'}</Table.Cell>
-      <Table.Cell>{budgetType}</Table.Cell>
+      <Table.Cell><BudgetType budgetType={budgetType} /></Table.Cell>
       <Table.Cell>{cid}</Table.Cell>
     </Table.Row>
   );
