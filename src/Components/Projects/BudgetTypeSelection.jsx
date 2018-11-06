ebf5c0ae1671fbdec150ffe7d0b754f980f1683c
Elly Maimon <eliranmaimon@gmail.com>
2018-11-06 10:36:05 -0800

    Add functionality to handle billing cycle selection using hooks,

---
+++ src/Components/Projects/BudgetTypeSelection.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Form } from 'semantic-ui-react';
+
+const formStyle = {
+  minWidth: "0",
+  maxWidth: "75%"
+}
+
+const options = [
+  { key: 'hourly', value: 'Hourly', text: 'Hourly' },
+  { key: 'monthly', value: 'Monthly', text: 'Monthly' },
+  { key: 'yearly', value: 'Yearly', text: 'Yearly' },
+  { key: 'fixed', value: 'Fixed', text: 'Fixed' }
+];
+
+const BudgetTypeSelection = ({ budgetType }) => {
+  const [state, setState] = useState(budgetType);
+
+  const handleChange = (e, selection) => {
+    setState(selection.value);
+  };
+
+  return (
+    <Form>
+      <Form.Select
+        onChange={handleChange}
+        options={options}
+        selection
+        value={state}
+        style={formStyle}
+      />
+    </Form>
+  );
+};
+
+export default BudgetTypeSelection;
