2327e7fae8a487afa950f318302722f3fc68ca00
Elly Maimon <eliranmaimon@gmail.com>
2018-11-06 14:30:02 -0800

    Add team members to mock data

--- src/Data/mockData.js
+++ src/Data/mockData.js
@@ -16,7 +16,6 @@ export const mockExpenses = [
   }
 ];
 
-
 export const mockProjects = [
   {
     id: 1,
@@ -56,5 +55,40 @@ export const mockProjects = [
     template: true,
     color: '5',
     budgetType: 'Hourly'
+  }
+];
+
+export const mockTeamMembers = [
+  {
+    id: 3123855,
+    uid: 35224123,
+    wid: 777,
+    admin: false,
+    active: false,
+    email: 'John@toggl.com',
+    at: '2013-05-17T16:50:36+03:00',
+    name: 'John Swift',
+    invite_url:
+      'https://toggl.com/user/accept_invitation?code=fb3ad3db5dasd123c2b529e3a519826'
+  },
+  {
+    id: 200066,
+    uid: 21961,
+    wid: 777,
+    admin: true,
+    active: true,
+    email: 'Jane@toggl.com',
+    at: '2012-04-11T22:59:33+03:00',
+    name: 'Jane'
+  },
+  {
+    id: 34534534,
+    uid: 21961,
+    wid: 777,
+    admin: true,
+    active: true,
+    email: 'mike@toggl.com',
+    at: '2012-04-11T22:59:33+03:00',
+    name: 'Michael Jackson'
   }
 ];
