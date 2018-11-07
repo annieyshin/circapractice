4e9034c7ed5323e8669f52a2323b070042c71be7
Elly Maimon <eliranmaimon@gmail.com>
2018-11-06 16:09:13 -0800

    Add handle submit function to the expenses form

--- src/Components/Expenses/ExpensesForm.jsx
+++ src/Components/Expenses/ExpensesForm.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import { Form } from 'semantic-ui-react';
+import { v4 } from 'uuid';
 
-const ExpensesForm = ({ handleCancel }) => {
+const ExpensesForm = ({ handleCancel, handleCreate }) => {
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    let submittedTitle = e.target.title.value;
+    let submittedCost = e.target.cost.value;
+
+    let newExpense = { id: v4(), title: submittedTitle, cost: submittedCost }
+
+    if (newExpense) {
+      handleCreate(newExpense);
+    }
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group>
-        <Form.Input placeholder="New Expense" type="text" width={4} />
-        <Form.Input placeholder="Cost" type="number" width={4} />
-        <Form.Button content="Submit" positive disabled />
+        <Form.Input
+          placeholder="New Expense"
+          name="title"
+          type="text"
+          width={4}
+        />
+        <Form.Input placeholder="Cost" name="cost" type="number" width={4} />
+        <Form.Button content="Submit" positive />
         <Form.Button onClick={handleCancel} content="Cancel" />
       </Form.Group>
     </Form>
