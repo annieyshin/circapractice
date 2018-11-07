3c6a47f1cd54fb45d162bba8403c42d0eb9ff3c1
Elly Maimon <eliranmaimon@gmail.com>
2018-11-07 12:36:50 -0800

    Clean up the test component by importing the hooks from a seperate file

--- src/Components/Test/ApiTest.jsx
+++ src/Components/Test/ApiTest.jsx
@@ -1,39 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import { init } from './token';
+import React  from 'react';
 import { Header } from 'semantic-ui-react';
+import { useWorkspace } from './ApiHooks'
 
 const ApiTest = () => {
   const workspace = useWorkspace();
 
   return (
     <div>
-      <Header>{workspace.id}</Header>
-      <Header>ID: {workspace.name}</Header>
+      <Header>{workspace.name}</Header>
+      <Header>ID: {workspace.id}</Header>
     </div>
   );
 };
 
 export default ApiTest;
 
+// -------------------------------------------------------------------------
+// Custom Hook reusable in other components (this is what we are importing)
+// -------------------------------------------------------------------------
+//
+// const useWorkspace = () => {
+//   const getWorkspacesUrl = `https://www.toggl.com/api/v8/workspaces`;
+//   const [workspaceId, setWorkspaceId] = useState(null);
+//   const [workspaceName, setWorkspaceName] = useState(null);
 
-//Custom Hook reusable in other components
-const useWorkspace = () => {
-  const getWorkspacesUrl = `https://www.toggl.com/api/v8/workspaces`;
-  const [workspaceId, setWorkspaceId] = useState(null);
-  const [workspaceName, setWorkspaceName] = useState(null);
+//   useEffect(() => {
+//     fetch(getWorkspacesUrl, init)
+//       .then(response => response.json())
+//       .then(data => {
+//         setWorkspaceId(data[0].id);
+//         setWorkspaceName(data[0].name);
+//         console.log(data);
+//       });
+//   }, []);
 
-  useEffect(() => {
-    fetch(getWorkspacesUrl, init)
-      .then(response => response.json())
-      .then(data => {
-        setWorkspaceId(data[0].id);
-        setWorkspaceName(data[0].name);
-        console.log(data);
-      });
-  }, []);
+//   return {
+//     id: workspaceId,
+//     name: workspaceName
+//   }
+// }
 
-  return {
-    id: workspaceId,
-    name: workspaceName
-  }
-}
+// --------------------------------------------
+// Custom Hook Example for reusable form input
+// --------------------------------------------
+//
+// const useFormInput = (initialValue) => {
+//   const [value, setValue] = useState(initialValue);
+
+//   const handleChange = e => setValue(e.target.value);
+
+//   return {
+//     value,
+//     onChange: handleChange
+//   }
+// }
