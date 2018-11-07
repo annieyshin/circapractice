ee93df124c70ca7c97a2dc7af9dce7f024e8e4c9
Elly Maimon <eliranmaimon@gmail.com>
2018-11-07 14:54:30 -0800

    Pass down props to newly rendered select components

--- src/Components/Reports/ReportsNav.jsx
+++ src/Components/Reports/ReportsNav.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { Menu } from 'semantic-ui-react';
 import TeamMemberSelect from './TeamMemberSelect';
 import ReportsSearch from './ReportsSearch';
+import ClientSelect from './ClientSelect';
+import ProjectsSelect from './ProjectsSelect';
 
 
-const ReportsNav = ({teamMembers}) => {
+const ReportsNav = ({teamMembers, clients, projects}) => {
   return (
     <Menu borderless>
       <TeamMemberSelect teamMembers={teamMembers} />
+      <ClientSelect clients={clients} />
+      <ProjectsSelect projects={projects} />
       <ReportsSearch />
     </Menu>
 
