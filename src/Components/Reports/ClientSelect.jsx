ff899420fedac7541102c3f9fdd0bfcbaf4350dc
Elly Maimon <eliranmaimon@gmail.com>
2018-11-07 14:53:24 -0800

    Add client select component in the vain of team member select

---
+++ src/Components/Reports/ClientSelect.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Select, Menu } from 'semantic-ui-react';
+
+const ClientSelect = ({ clients }) => {
+  const getOptions = clients => {
+    let options = [];
+    clients.forEach(client => {
+      let option = {
+        key: client.id,
+        value: client.name,
+        text: client.name
+      };
+      options.push(option);
+    });
+    return options;
+  };
+
+  const options = getOptions(clients);
+
+  const [selection, setSelection] = useState(null);
+
+  const handleChange = (e, {value}) => {
+    setSelection(value);
+  };
+
+  console.log(`Current selection is: ${selection}`);
+
+  return (
+    <Menu.Item>
+      <Select
+        onChange={handleChange}
+        options={options}
+        placeholder="Client"
+        value={selection}
+      />
+    </Menu.Item>
+  );
+};
+
+export default ClientSelect;
