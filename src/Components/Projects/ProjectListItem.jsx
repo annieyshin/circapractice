65a0b680f71d37c5a4d5de5e527095177c48986d
Elly Maimon <eliranmaimon@gmail.com>
2018-11-08 12:28:33 -0800

    Create method to extract client name from clients prop

--- src/Components/Projects/ProjectListItem.jsx
+++ src/Components/Projects/ProjectListItem.jsx
@@ -2,14 +2,24 @@ import React from 'react';
 import { Table } from 'semantic-ui-react';
 import BillingTypeSelect from './BillingTypeSelect';
 
-const ProjectListItem = ({ project }) => {
+const ProjectListItem = ({ project, clients }) => {
   const { name, billable, budgetType, cid } = project;
+  let clientName = 'None';
+
+  clients.forEach(client => {
+    if (client.id === cid) {
+      clientName = client.name;
+    }
+  });
+
   return (
     <Table.Row>
       <Table.Cell>{name}</Table.Cell>
-      <Table.Cell textAlign='center'>{billable ? 'Yes' : 'No'}</Table.Cell>
-      <Table.Cell>{cid ? `${cid}` : "None ya"}</Table.Cell>
-      <Table.Cell><BillingTypeSelect budgetType={budgetType} /></Table.Cell>
+      <Table.Cell textAlign="center">{billable ? 'Yes' : 'No'}</Table.Cell>
+      <Table.Cell>{clientName ? clientName : 'None ya'}</Table.Cell>
+      <Table.Cell>
+        <BillingTypeSelect budgetType={budgetType} />
+      </Table.Cell>
     </Table.Row>
   );
 };
