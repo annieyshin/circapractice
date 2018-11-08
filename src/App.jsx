7134c297c218d4a7cf3ab3f348bd9147a9fe3d1e
Elly Maimon <eliranmaimon@gmail.com>
2018-11-08 12:46:19 -0800

    Remove test area route from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,41 @@
-import React from "react";
-import { Container } from "semantic-ui-react";
-import { Route, Switch } from "react-router-dom";
-import HomePage from "./Components/HomePage";
-import NavBar from "./Components/NavBar";
-import ReportsMain from "./Components//Reports/ReportsMain";
-import ExpensesMain from './Components/Expenses/ExpensesMain'
-import ProjectsMain from "./Components/Projects/ProjectsMain";
-import ApiTest from "./Components/TestArea/ApiTest";
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './Components/HomePage';
+import NavBar from './Components/NavBar';
+import ReportsMain from './Components//Reports/ReportsMain';
+import ExpensesMain from './Components/Expenses/ExpensesMain';
+import ProjectsMain from './Components/Projects/ProjectsMain';
 import { useData } from './Components/Common/ApiHooks';
 
 const App = () => {
-    const data = useData();
-    return (
-      <div>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-        </Switch>
+  const data = useData();
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
 
-        <Route
-          path="/(.+)"
-          render={() => (
-            <div>
-              <NavBar />
-              <Container className="main">
-                <Switch>
-                  <Route path="/reports" component={ReportsMain} />
-                  <Route path="/expenses" component={ExpensesMain} />
-                  <Route path="/projects" render={()=><ProjectsMain data={data}/>} />
-                  <Route path="/test" component={ApiTest} />
-                </Switch>
-              </Container>
-            </div>
-          )}
-        />
-      </div>
-    );
-}
+      <Route
+        path="/(.+)"
+        render={() => (
+          <div>
+            <NavBar />
+            <Container className="main">
+              <Switch>
+                <Route path="/reports" component={ReportsMain} />
+                <Route path="/expenses" component={ExpensesMain} />
+                <Route
+                  path="/projects"
+                  render={() => <ProjectsMain data={data} />}
+                />
+              </Switch>
+            </Container>
+          </div>
+        )}
+      />
+    </div>
+  );
+};
 
 export default App;
