964cd4cdaad6ff363d135a2d18fbda2e105d7e63
Elly Maimon <eliranmaimon@gmail.com>
2018-11-08 11:36:12 -0800

    Remove comment

--- src/Components/TestArea/ApiTest.jsx
+++ src/Components/TestArea/ApiTest.jsx
@@ -15,31 +15,6 @@ const ApiTest = () => {
 
 export default ApiTest;
 
-// -------------------------------------------------------------------------
-// Custom Hook reusable in other components (this is what we are importing)
-// -------------------------------------------------------------------------
-//
-// const useWorkspace = () => {
-//   const getWorkspacesUrl = `https://www.toggl.com/api/v8/workspaces`;
-//   const [workspaceId, setWorkspaceId] = useState(null);
-//   const [workspaceName, setWorkspaceName] = useState(null);
-
-//   useEffect(() => {
-//     fetch(getWorkspacesUrl, init)
-//       .then(response => response.json())
-//       .then(data => {
-//         setWorkspaceId(data[0].id);
-//         setWorkspaceName(data[0].name);
-//         console.log(data);
-//       });
-//   }, []);
-
-//   return {
-//     id: workspaceId,
-//     name: workspaceName
-//   }
-// }
-
 // --------------------------------------------
 // Custom Hook Example for reusable form input
 // --------------------------------------------
